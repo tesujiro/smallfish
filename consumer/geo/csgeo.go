9985fbaf19852f259ad9444b20475283b27db496
tesujiro <tesujiro@gmail.com>
2018-02-25 07:21:35 +0900

    add: consumer handler

--- consumer/geo/csgeo.go
+++ consumer/geo/csgeo.go
@@ -7,10 +7,10 @@ import (
 	"net/http"
 	"strconv"
 	"time"
+
+	"github.com/gorilla/mux"
 )
 
-// http.HandleFuncに登録する関数
-// http.ResponseWriterとhttp.Requestを受ける
 func Sleeper(w http.ResponseWriter, r *http.Request) {
 	q := r.URL.Query()
 	i, _ := strconv.Atoi(q.Get("timer"))
@@ -18,11 +18,22 @@ func Sleeper(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, World :slept %d msec\n", i)
 }
 
+func ConsumerHandler(w http.ResponseWriter, r *http.Request) {
+	log.Printf("ConsumerHandler")
+	vars := mux.Vars(r)
+
+	fmt.Fprintf(w, "URL=%v\n", r.URL)
+	fmt.Fprintf(w, "Vars=%v\n", vars)
+}
+
 func main() {
 	port := flag.Int("port", 80, "port number")
 	flag.Parse()
 
-	http.HandleFunc("/", Sleeper)
+	r := mux.NewRouter()
+	r.HandleFunc("/", Sleeper)
+	r.HandleFunc("/consumer/@{longtitude:[0-9]+},{latitude:[0-9]+}", ConsumerHandler)
+	http.Handle("/", r)
 
 	log.Printf("Start Go HTTP Server")
 
