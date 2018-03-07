70c478e2fe5032a1910ae65a0db31c83743bb7a2
tesujiro <tesujiro@gmail.com>
2018-03-08 05:32:56 +0900

    add: grant to db_user

--- database/db_create.go
+++ database/db_create.go
@@ -10,19 +10,20 @@ import (
 
 const db_port = 30257
 const db_host = "localhost"
-const db_user = "root"
+const db_owner = "root"
+const db_user = "maxroach"
 const db_consumer_geo = "consumer_geo"
 
 func main() {
 	// Connect to the database.
-	url := fmt.Sprintf("postgresql://%s@%s:%d/%s?sslmode=disable", db_user, db_host, db_port, db_consumer_geo)
+	url := fmt.Sprintf("postgresql://%s@%s:%d/%s?sslmode=disable", db_owner, db_host, db_port, db_consumer_geo)
 	db, err := sql.Open("postgres", url)
 	if err != nil {
 		log.Fatal("error connecting to the database: ", err)
 		fmt.Println("error connecting to the database: ", err)
 	}
 	defer db.Close()
-	fmt.Println("sql.Open finished!!")
+	//fmt.Println("sql.Open finished!!")
 
 	objs := []struct {
 		class      string
@@ -43,8 +44,21 @@ func main() {
 		ddl := fmt.Sprintf("CREATE %s IF NOT EXISTS %s %s", obj.class, obj.name, obj.definition)
 		if _, err := db.Exec(ddl); err != nil {
 			fmt.Printf("create %v %v failed!!\n", obj.class, obj.name)
-			log.Fatal(err)
+			log.Print(err)
+		} else {
+			fmt.Printf("create %v %v finished :%v \n", obj.class, obj.name, obj.definition)
 		}
-		fmt.Printf("create %v %v finished :%v \n", obj.class, obj.name, obj.definition)
+	}
+
+	ddls := []string{
+		"GRANT ALL ON DATABASE " + db_consumer_geo + " TO " + db_user,
+	}
+	for _, ddl := range ddls {
+		if _, err := db.Exec(ddl); err != nil {
+			fmt.Printf("failed!! :%v\n", ddl)
+			log.Print(err)
+		} else {
+			fmt.Printf("finished: %v\n", ddl)
+		}
 	}
 }
