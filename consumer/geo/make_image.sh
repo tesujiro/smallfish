7637822472fe5771f1f044ebb8b51d2fc9ff6883
tesujiro <tesujiro@gmail.com>
2018-03-11 21:04:17 +0900

    deploy for development

--- consumer/geo/make_image.sh
+++ consumer/geo/make_image.sh
@@ -5,4 +5,8 @@ docker tag $MODULE localhost:5000/$MODULE
 docker push localhost:5000/$MODULE
 docker rmi localhost:5000/$MODULE
 docker pull localhost:5000/$MODULE
+for p in `kubectl get pod | grep $MODULE | awk '{print $1}'`
+do
+	kubectl delete pod $p
+done
 
