b560fd1456293bd7166ce305c8cf733575330ef5
tesujiro <tesujiro@gmail.com>
2018-03-24 15:12:54 +0900

    refact: delete databse logic

--- consumer/geo/config.go
+++ consumer/geo/config.go
@@ -2,20 +2,11 @@ package csgeo
 
 import "flag"
 
-const db_host_default = "localhost"
-const db_port_default = 30257
-const db_user = "maxroach"
 const kafka_host_default = "localhost"
 const kafka_port_default = 31090
 
-//const db_host = "cockroachdb-public"
-
 type Config struct {
 	http_port  int
-	db_host    string
-	db_port    int
-	db_user    string
-	db_name    string
 	kafka_host string
 	kafka_port int
 }
@@ -23,17 +14,11 @@ type Config struct {
 func NewConfig() (*Config, error) {
 	c := &Config{}
 	port := flag.Int("port", 80, "port number")
-	dbserver := flag.String("dbserver", db_host_default, "database server")
-	dbport := flag.Int("dbport", db_port_default, "database port")
 	kafka_server := flag.String("kafka_server", kafka_host_default, "kafka server")
 	kafka_port := flag.Int("kafka_port", kafka_port_default, "kafka port")
 	flag.Parse()
 
 	c.http_port = *port
-	c.db_host = *dbserver
-	c.db_port = *dbport
-	c.db_user = db_user
-	c.db_name = "consumer_geo"
 	c.kafka_host = *kafka_server
 	c.kafka_port = *kafka_port
 	return c, nil
--- consumer/geo/csgeo.go
+++ consumer/geo/csgeo.go
@@ -2,7 +2,6 @@ package csgeo
 
 import (
 	"context"
-	"database/sql"
 	"fmt"
 	"io"
 	"log"
@@ -37,78 +36,6 @@ type ConsumerGeoInfo struct {
 	Lat        float64   `json:"latitude"`
 	Lng        float64   `json:"longtitude"`
 }
-
-func (c *Consumer) connect() (*sql.DB, error) {
-	url := fmt.Sprintf("postgresql://%s@%s:%d/%s?sslmode=disable", c.config.db_user, c.config.db_host, c.config.db_port, c.config.db_name)
-	db, err := sql.Open("postgres", url)
-	if err != nil {
-		//log.Fatal("error connecting to the database: ", err)
-		fmt.Println("error connecting to the database: ", err)
-	}
-	return db, err
-}
-
-func (c *Consumer) addConsumerGeo(db *sql.DB, geo ConsumerGeoInfo) error {
-	// Insert two rows into the "location" table.
-	stmt, err := db.Prepare("INSERT INTO location (id, time, lat, lng) VALUES ($1,$2,$3,$4)")
-	if err != nil {
-		log.Printf("prepare statement faled!! %v", err)
-		return err
-	}
-	defer stmt.Close() // danger!
-
-	res, err := stmt.Exec(geo.ConsumerId, geo.Timestamp, geo.Lat, geo.Lng)
-	if err != nil {
-		log.Printf("exec statement faled!!")
-		return err
-	}
-
-	/*
-		lastId, err := res.LastInsertId()
-		if err != nil {
-			log.Fatal(err)
-		}
-		log.Printf("ID = %d, affected = %d\n", lastId, rowCnt)
-	*/
-	rowCnt, err := res.RowsAffected()
-	if err != nil {
-		log.Printf("get rows affected faled!!")
-		return err
-	}
-	log.Printf("affected = %d\n", rowCnt)
-
-	return nil
-}
-
-/*
-func ConsumerHandler(w http.ResponseWriter, r *http.Request) {
-	log.Printf("ConsumerHandler!!")
-	vars := mux.Vars(r)
-	lat, err := strconv.ParseFloat(vars["latitude"], 64)
-	if err != nil {
-		log.Fatal(err)
-		return
-	}
-	lng, err := strconv.ParseFloat(vars["longtitude"], 64)
-	if err != nil {
-		log.Fatal(err)
-		return
-	}
-
-	geo := ConsumerGeoInfo{Lat: lat, Lng: lng}
-
-	// CHU RYAKU!!
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	//w.WriteHeader(http.StatusNotFound)
-
-	if err := json.NewEncoder(w).Encode(geo); err != nil {
-		log.Print("json.NewEncoder error!\n")
-		log.Fatal(err)
-	}
-}
-*/
 
 const topic = "new_topic"
 
@@ -177,54 +104,10 @@ func (c *Consumer) GeoCollectionWriter(w http.ResponseWriter, r *http.Request) {
 	log.Printf("Content-Length:%v", length)
 
 	//Kafka
-	//c.KafkaProduce(fmt.Sprintf("%v %v", time.Now(), r.RemoteAddr), fmt.Sprintf("%v", geos))
-	//c.KafkaProduce(r.RemoteAddr, fmt.Sprintf("%v", geos))
-	c.KafkaProduce(r.RemoteAddr, string(body))
+	if err := c.KafkaProduce(r.RemoteAddr, string(body)); err != nil {
+		log.Printf("Kafka produce failed!!")
+	}
 
-	/*
-		var geos []ConsumerGeoInfo
-		err = json.Unmarshal(body[:length], &geos)
-		if err != nil {
-			log.Printf("json.Unmarshal failed!! %v", err)
-			log.Printf("json:%s", body[:length])
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-
-
-		//database
-		db, err := c.connect()
-		if err != nil {
-			log.Printf("database connect failed!!")
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-
-		tx, err := db.Begin()
-		if err != nil {
-			log.Printf("transaction begin failed!!")
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-		defer tx.Rollback()
-
-		for _, geo := range geos {
-			log.Printf("%v\n", geo)
-			if err := c.addConsumerGeo(db, geo); err != nil {
-				log.Fatal(err)
-				w.WriteHeader(http.StatusInternalServerError)
-				return
-			}
-		}
-
-		err = tx.Commit()
-		if err != nil {
-			log.Printf("commit faled!!")
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-
-	*/
 	w.WriteHeader(http.StatusOK)
 }
 
